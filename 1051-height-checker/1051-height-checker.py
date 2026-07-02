06f9018ac0de0160d872f7da95ff93509fb4198d
Deepak04102 <sonuk82975@gmail.com>
2026-07-02 22:51:33 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.52%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def heightChecker(self, heights):
+        a=sorted(heights)
+        count=0
+        for i in range(len(heights)):
+           
+
+               if heights[i]!=a[i]:
+                count+=1
+        return count
+                
+
+        
